b02ae57535ce89f3390e065d10535e164baf0cbb
nyksy <71010049+nyksy@users.noreply.github.com>
2022-10-16 19:44:51 +0300

    added react-leaflet & basic map

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# SUSE-Group-Project
+# SUSE-Group-Project, Ahti AI -application
+
+## Using location data and image recognition to improve sustainability in fishing
+
+[classifier readme](./classifier/README.md)
--- client/src/components/pages/Map/Map.js
+++ client/src/components/pages/Map/Map.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  WMSTileLayer,
+  Popup,
+  useMapEvents,
+  Marker,
+} from "react-leaflet";
+import "./Map.css";
 
 function Map() {
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  }
+
   return (
     <>
-      Map page content
+      <MapContainer center={[62.60118, 29.76316]} zoom={13} id="map">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <WMSTileLayer
+          layers={"kalastusrajoitus"}
+          transparent={true}
+          format={"image/png"}
+          url="http://avoinkara.mmm.fi:80/geoserver/ows?SERVICE=WMS&"
+        />
+        <Marker position={[62.60118, 29.76316]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <LocationMarker />
+      </MapContainer>
     </>
   );
 }
